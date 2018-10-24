118ef4989a955439648eeeef83be3227f68c74d2
Jason Hrabi <jason.hrabi@microfocus.com>
2018-10-24 12:27:10 -0400

    COLLATE using uft8_bin

--- 5.7/docker-entrypoint.sh
+++ 5.7/docker-entrypoint.sh
@@ -174,7 +174,7 @@ if [ "$1" = 'mysqld' -a -z "$wantHelp" ]; then
 
 		file_env 'MYSQL_DATABASE'
 		if [ "$MYSQL_DATABASE" ]; then
-			echo "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` CHARACTER SET utf8 COLLATE utf8_general_ci;" | "${mysql[@]}"
+			echo "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` CHARACTER SET utf8 COLLATE utf8_bin;" | "${mysql[@]}"
 			mysql+=( "$MYSQL_DATABASE" )
 		fi
 
